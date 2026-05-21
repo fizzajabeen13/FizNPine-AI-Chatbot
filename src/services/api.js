c41fedbb525b80a819f170804ffa809cb4d85834
fizzajabeen13 <fizzajabeen13@gmail.com>
2026-05-21 11:36:12 +0500

    connect frontend with backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://fiz-n-pine-ai-1ptd.vercel.app/api/chat",
 });
 
 // Send message to backend
